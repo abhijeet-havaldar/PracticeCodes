4de9e0d22db86085dc80a40e76ee23c757d6a0ab
Abhijeet Havaldar <abhijeethavaldar7781@gmail.com>
2025-10-26 22:31:44 +0530

    Add files via upload

---
+++ 49.py
@@ -0,0 +1,12 @@
+#Q:Write a Python program that reads the two adjoining sides and the diagonal of a parallelogram and checks whether the parallelogram is a rectangle or a rhombus.
+
+print("Input two adjoined sides and the diagonal of a parallelogram (comma separated):")
+
+a, b, c = map(int, input().split(","))
+
+if c**2 == a**2 + b**2:
+    print("This is a rectangle.")
+
+if a ==b:
+    print("This is a rhombus.")
+
